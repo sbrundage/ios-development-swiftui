b2fd6fb8494e7bc46af8f73fa1b00665b59b62ad
Stephen Brundage <stephen.brundage12@gmail.com>
2022-02-08 08:01:20 -0500

    Initial Commit - Project Setup
    Import all assets / resources downloaded from project video.
    Create project folders and set project settings
     - Launch Screen
     - Portrait mode only

---
+++ Touchdown/Touchdown/App/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Stephen Brundage on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Touchdown/Touchdown/Utility/Contants.swift
@@ -0,0 +1,16 @@
+//
+//  Contants.swift
+//  Touchdown
+//
+//  Created by Stephen Brundage on 2/8/22.
+//
+
+import SwiftUI
+
+// COLORS
+
+let colorBackground = Color("ColorBackground")
+let colorGray = Color(UIColor.systemGray4)
+
+
+
